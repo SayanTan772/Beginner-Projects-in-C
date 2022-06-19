7d18282371795265d8382f6cf6d2db604a35b9b6
Sayantan Dey <92203629+SayanTan772@users.noreply.github.com>
2022-06-19 15:54:36 +0530

    fixed user input format error

--- Digital_Clock.c
+++ Digital_Clock.c
@@ -8,14 +8,20 @@ int main()
   int h,m,s;
   printf("Set the time (hh:mm:ss) \n");
   scanf("%d %d %d",&h,&m,&s); // hour , minute and second
-  system("cls"); // clears the screen for displaying the time
-  while(start!=0) // start will never be 0 so the loop runs for infinity unless we terminate the output 
+  if(h==60 || m==60 || s==0)
   {
+     printf("Wrong format \n");
+  }
+  else
+  {
+    system("cls"); // clears the screen for displaying the time
+    while(start!=0) // start will never be 0 so the loop runs for infinity unless we terminate the output 
+   {
     printf("Digital Clock: \n");
     printf("\n");
     printf("%02d : %02d : %02d ",h,m,s); // current values of h m and s are printed 
     if(s==59)
-    {
+     {
        if(m!=59)
         {
           m++; // we keep on incrementing the minute until it becomes 59
@@ -26,13 +32,14 @@ int main()
           h++; // we increment the hour by 1
         }
         s=0; // as soon as second becomes 59 it starts from 0 all over again for another 60 seconds and this is repeated 
-    }
+      }
     else
-    {
+     {
       s++; // we keep on incrementing the second until it becomes 59	
 	}
     Sleep(1000); // pauses the screen for 1 sec
     system("cls"); // clears the screen after every second so that updated time can be printed in the next itertation
-  }
+     }
+   }
   return 0;
 }
